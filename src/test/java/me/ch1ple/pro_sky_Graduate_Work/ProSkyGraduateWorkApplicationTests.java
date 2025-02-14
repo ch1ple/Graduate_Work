1eb06d69513a90cc3540b3cdc6daf582f8b9188c
ch1ple <teavis@mail.ru>
2025-02-14 23:10:30 +0300

    initial commit

---
+++ src/test/java/me/ch1ple/pro_sky_Graduate_Work/ProSkyGraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ch1ple.pro_sky_Graduate_Work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSkyGraduateWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
